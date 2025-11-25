c78c97decc7c4f8303b5afad3f9db2828519b0c4
Dani Diestre <dani.diestre@gmail.com>
2025-11-25 10:56:50 +0100

    sold out rooms now can click

--- src/lib/room-data.ts
+++ src/lib/room-data.ts
@@ -207,7 +207,7 @@ export const floorsData: FloorData[] = [
         number: "Habitación 4",
         capacity: 2,
         hasPrivateBathroom: true,
-        isSoldOut: true,
+        isSoldOut: false,
         floor: 1,
         bedType: "2 single beds",
         bathroomNumber: 4,
