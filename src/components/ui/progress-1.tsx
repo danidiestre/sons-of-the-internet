32ec8ca369a70fa4d34c8c4ec83a05726c317761
adrixo <adrianvalrom@gmail.com>
2026-01-03 14:05:23 +0100

    feat(progress-line): fix the button space

--- src/components/ui/progress-1.tsx
+++ src/components/ui/progress-1.tsx
@@ -29,7 +29,7 @@ export default function ExampleProgress({ value = 55 }: { value?: number }) {
             <Progress.Indicator className="block bg-white transition-all duration-500" />
           </Progress.Track>
         </Progress.Root>
-        <div className="flex justify-center">
+        <div className="flex justify-center pt-5">
           <div className="relative group inline-block">
           <div className="absolute inset-0 -m-2 rounded-full hidden sm:block bg-gray-100 opacity-40 filter blur-lg pointer-events-none transition-all duration-300 ease-out group-hover:opacity-60 group-hover:blur-xl group-hover:-m-3"></div>
           <a href="https://tally.so/r/n025Aj" target="_blank" rel="noopener noreferrer" className="relative z-10 px-6 py-3 text-sm font-semibold text-black bg-gradient-to-br from-gray-100 to-gray-300 rounded-full hover:from-gray-200 hover:to-gray-400 transition-all duration-200 cursor-pointer">
